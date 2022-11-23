a6c264cd6d79ce033112fbc2a61bca03dd7cfc91
Gisleudo Cortez <57629696+Gisleudo-Cortez@users.noreply.github.com>
2022-11-23 08:27:37 -0300

    programa de exemplo de utilização

---
+++ exemplo.py
@@ -0,0 +1,20 @@
+# importação dos módulos, disponibilizando as funções nesse programa
+import capacitor
+import indutor
+
+# utilização das funções importadas
+# voltagem_capacitor 
+vc = 40
+# tau
+tau = 0.560
+# intervalos de tempo
+t = 5
+
+# plotagem da carga em função do tempo
+capacitor.carga_em_funcao_do_tempo_plotagem(vc, tau, t)
+
+# plotagem da resposta transitoria da corrente indutor
+E = 50
+R = 2000
+tau = 0.002
+indutor.resposta_transitoria_corrente_plotagem(E, R, tau, t)
